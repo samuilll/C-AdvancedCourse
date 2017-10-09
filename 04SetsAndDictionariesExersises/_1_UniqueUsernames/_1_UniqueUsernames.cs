fd0d4771bc3ecd13cbdef260728214931c0088ba
samuilll <samuil_dechev@abv.bg>
2017-10-09 19:28:08 +0300

    Commit

--- 04SetsAndDictionariesExersises/_1_UniqueUsernames/_1_UniqueUsernames.cs
+++ 04SetsAndDictionariesExersises/_1_UniqueUsernames/_1_UniqueUsernames.cs
@@ -8,6 +8,22 @@ class _1_UniqueUsernames
 {
     static void Main(string[] args)
     {
+
+        int numberOfLines = int.Parse(Console.ReadLine());
+
+         HashSet<string> namesList = new HashSet<string>();
+
+        for (int i = 0; i < numberOfLines; i++)
+        {
+            string name = Console.ReadLine();
+
+            namesList.Add(name);
+        }
+
+        foreach (var name in namesList)
+        {
+            Console.WriteLine(name);
+        }
     }
 }
 
